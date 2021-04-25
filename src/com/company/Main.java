e61218d58f2483356327276a40d053558abe315a
Akash CP <57165028+AkashcpApps@users.noreply.github.com>
2021-04-26 01:58:15 +0530

    commit for test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code here code here
+	    for(int i=0;i<10;i++)
+	    {
 
         System.out.println("Test");
+	    }
     }
 }
